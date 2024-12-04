f6f75e3b81f7fc1e17d95ba43cba831dd3440502
Omid Bodaghi <42227752+omibo@users.noreply.github.com>
2024-12-04 12:57:33 -0800

    Add basic functionalities required for FundMe

--- FundMe.sol
+++ FundMe.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
 
+contract FundMe {
+
+    function fund() public payable {
+        require (msg.value > 1e18, "Did no send enough ETH");
+    }
+
+    function withdraw() public {}
+
+}
