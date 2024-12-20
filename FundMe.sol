38cd1cc3b48b5415762fb83e0057c02e92232d22
Omid Bodaghi <42227752+omibo@users.noreply.github.com>
2024-12-20 12:07:44 -0800

    Update FundMe.sol
    Add ETH/USD Price Getter from Chainlink

--- FundMe.sol
+++ FundMe.sol
@@ -1,11 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 
 contract FundMe {
+
+    uint256 public minimumUSD = 5;
 
     function fund() public payable {
         require (msg.value > 1e18, "Did no send enough ETH");
     }
+
+    function get_price() public view returns(uint256) {
+        AggregatorV3Interface price_feed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+        (, int256 price, , , ) = price_feed.latestRoundData();
+        return uint256(price * 1e10);
+
+    }
+
+    function get_conversion_rate() public {}
 
     function withdraw() public {}
 
